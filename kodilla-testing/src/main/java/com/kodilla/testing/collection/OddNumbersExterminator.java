744b1ed34c15a5f8adb2018ae01dec347a32fb9d
Bartosz Kedziora <bartosz.kedziora89@gmail.com>
2020-11-12 13:46:43 +0100

    Create two tests

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -1,2 +1,25 @@
-package com.kodilla.testing.collection;public class OddNumbersExterminator {
+package com.kodilla.testing.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OddNumbersExterminator
+{
+    public List<Integer> exterminate(List<Integer> numbers)
+    {
+        if(numbers.isEmpty())
+        {
+            numbers=null;
+            return numbers;
+        }
+        List<Integer> oddNumbers = new ArrayList<>();
+        for (Integer number: numbers)
+        {
+            if(number%2==0)
+            {
+                oddNumbers.add(number);
+            }
+        }
+        return oddNumbers;
+    }
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,2 +1,66 @@
-package com.kodilla.testing.collection;public class CollectionTestSuite {
+package com.kodilla.testing.collection;
+
+import org.junit.jupiter.api.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@DisplayName("Collection test Suite")
+class CollectionTestSuite
+{
+    private String methodName;
+    public String getMethodName()
+    {
+        return methodName;
+    }
+
+    public void setMethodName(String methodName)
+    {
+        this.methodName = methodName;
+    }
+
+    @BeforeEach
+    public void before()
+    {
+        System.out.println("Test begin");
+    }
+
+    @AfterEach
+    public void after()
+    {
+        System.out.println("Test end");
+    }
+
+    @DisplayName("A test to check if a class behaves correctly when the list is empty")
+    @Test
+    void testOddNumbersExterminatorEmptyList()
+    {
+        setMethodName(new Throwable().getStackTrace()[0].getMethodName());
+        System.out.println(getMethodName());
+        //Given
+        OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
+        //When
+        List<Integer> oddNumbers = new ArrayList<>();
+        System.out.println("Testing, List is empty");
+        //Then
+        Assertions.assertNull(oddNumbersExterminator.exterminate(oddNumbers));
+    }
+
+    @DisplayName("A test to check if a class behaves correctly when the list contains even and odd numbers")
+    @Test
+    void testOddNumbersExterminatorNormalList()
+    {
+        setMethodName(new Throwable().getStackTrace()[0].getMethodName());
+        System.out.println(getMethodName());
+        //Given
+        OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
+        //When
+        List<Integer> allNumbers;
+        Integer[] a = new Integer[] {7,8};
+        allNumbers = Arrays.asList(a);
+        allNumbers=oddNumbersExterminator.exterminate(allNumbers);
+        System.out.println("Testing, List contain odd number: 8");
+        //Then
+        Assertions.assertEquals(8,allNumbers.get(0));
+    }
 }
