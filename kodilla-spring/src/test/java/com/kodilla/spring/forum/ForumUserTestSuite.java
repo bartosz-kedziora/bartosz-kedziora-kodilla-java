cccb6deb1e0806ee482b2360d8ccae38837b30ac
Bartosz Kedziora <bartosz.kedziora89@gmail.com>
2021-01-13 22:58:23 +0100

    14.1 - bean first fight

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
@@ -0,0 +1,27 @@
+package com.kodilla.spring.forum;
+
+import com.kodilla.forum.ForumUser;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class ForumUserTestSuite
+{
+    @Test
+    void testGetUsername()
+    {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.forum");
+
+        //When
+        String name = context.getBean(ForumUser.class).getUsername();
+
+        //Then
+        assertEquals("John Smith", name);
+    }
+}
