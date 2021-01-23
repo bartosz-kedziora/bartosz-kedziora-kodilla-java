b4605653eabe53005c66e681be61e212a5496924
Bartosz Kedziora <bartosz.kedziora89@gmail.com>
2021-01-23 12:58:40 +0100

    15.5/15.6

--- kodilla-patterns/src/main/java/com/kodilla/patterns/factory/tasks/TaskFactory.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/factory/tasks/TaskFactory.java
@@ -6,11 +6,15 @@ public class TaskFactory {
     public static final String DRIVING_TASK = "DRIVING";
 
     public final Task makeTask(final String taskClass) {
-        return switch (taskClass) {
-            case SHOPPING_TASK -> new ShoppingTask("Shopping List", "Beers", 7);
-            case PAINTING_TASK -> new PaintingTask("Painitng challenge", "Black", "Masterpiece");
-            case DRIVING_TASK -> new DrivingTask("Holiday route", "USA", "Car");
-            default -> null;
-        };
+        switch (taskClass) {
+            case SHOPPING_TASK:
+                return new ShoppingTask("Shopping List", "Beers", 7);
+            case PAINTING_TASK:
+                return new PaintingTask("Painitng challenge", "Black", "Masterpiece");
+            case DRIVING_TASK:
+                return new DrivingTask("Holiday route", "USA", "Car");
+            default:
+                return null;
+        }
     }
 }
