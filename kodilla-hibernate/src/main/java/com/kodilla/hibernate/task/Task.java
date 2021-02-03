de2f22a2c4d4d5bc1ec8b137c944016d6880111f
Bartosz Kedziora <bartosz.kedziora89@gmail.com>
2021-02-03 21:00:55 +0100

    Revert "17.3 hibernate error"
    This reverts commit cd8f0b165b8ceec6ca07abb4dbcc654cf12588fe.

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
@@ -12,8 +12,6 @@ public final class Task {
     private String description;
     private Date created;
     private int duration;
-    private TaskFinancialDetails taskFinancialDetails;
-    private TaskList taskList;
 
     public Task() {
     }
@@ -48,39 +46,19 @@ public final class Task {
         return duration;
     }
 
-    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    @JoinColumn(name= "TASKS_FINANCIALS_ID")
-    public TaskFinancialDetails getTaskFinancialDetails() {
-        return taskFinancialDetails;
-    }
-
-    public void setTaskFinancialDetails(TaskFinancialDetails taskFinancialDetails) {
-        this.taskFinancialDetails = taskFinancialDetails;
-    }
-
-    @ManyToOne
-    @JoinColumn(name = "TASKLISTS_ID")
-    public TaskList getTaskList() {
-        return taskList;
-    }
-
-    public void setTaskList(TaskList taskList) {
-        this.taskList = taskList;
-    }
-
-    public void setId(int id) {
+    private void setId(int id) {
         this.id = id;
     }
 
-    public void setDescription(String description) {
+    private void setDescription(String description) {
         this.description = description;
     }
 
-    public void setCreated(Date created) {
+    private void setCreated(Date created) {
         this.created = created;
     }
 
-    public void setDuration(int duration) {
+    private void setDuration(int duration) {
         this.duration = duration;
     }
 }
--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
@@ -1,71 +1,51 @@
 package com.kodilla.hibernate.tasklist;
 
-import com.kodilla.hibernate.task.Task;
-
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
-@Table(name="TASKLISTS")
-public class TaskList {
+@Table(name = "TASKLISTS")
+public final class TaskList {
 
     private int id;
     private String listName;
     private String description;
-    private List<Task> tasks = new ArrayList<>();
 
     public TaskList() {
     }
 
-    public TaskList(String listName, String description) {
-        this.listName = listName;
+    public TaskList(String description, String listName) {
         this.description = description;
+        this.listName = listName;
     }
 
     @Id
-    @NotNull
     @GeneratedValue
-    @Column(name="ID", unique=true)
+    @NotNull
+    @Column(name = "ID", unique = true)
     public int getId() {
         return id;
     }
 
-    @NotNull
+    public void setId(int id) {
+        this.id = id;
+    }
+
     @Column(name="LISTNAME")
     public String getListName() {
         return listName;
-    }
-
-    @Column(name = "DESCRIPTION")
-    public String getDescription() {
-        return description;
-    }
-
-    @OneToMany(
-            targetEntity = Task.class,
-            mappedBy = "taskList",
-            cascade = CascadeType.ALL,
-            fetch = FetchType.LAZY
-    )
-    public List<Task> getTasks() {
-        return tasks;
-    }
-
-    private void setId(int id) {
-        this.id = id;
     }
 
     private void setListName(String listName) {
         this.listName = listName;
     }
 
-    private void setDescription(String description) {
-        this.description = description;
+    @Column(name = "DESCRIPTION")
+    private String getDescription() {
+        return description;
     }
 
-    private void setTasks(List<Task> tasks) {
-        this.tasks = tasks;
+    private void setDescription(String description) {
+        this.description = description;
     }
 }
