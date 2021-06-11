3f443b10ef317ea5d270587d506ae750f4d93cb3
Bartosz Kedziora <bartosz.kedziora89@gmail.com>
2021-06-11 23:03:17 +0200

    31.2

--- kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -1,2 +1,37 @@
+package com.kodilla.testing2.facebook;
 
+import com.kodilla.testing2.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class FacebookTestingApp {
+
+    public static String XPATH_WAIT_FOR = "//select[1]";
+    public static String XPATH_SELECT_DAY = "//div[@class=\"_5k_5\"]/span/span/select[1]";
+    public static String XPATH_SELECT_MONTH = "//div[@class=\"_5k_5\"]/span/span/select[2]";
+    public static String XPATH_SELECT_YEAR = "//div[@class=\"_5k_5\"]/span/span/select[3]";
+
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME)
+                .orElseThrow(() -> new RuntimeException("No such driver exception"));
+        driver.get("https://www.facebook.com/r.php?locale=pl_PL");
+
+        while(!driver.findElement(By.xpath(XPATH_WAIT_FOR)).isDisplayed());
+
+        WebElement selectDay = driver.findElement(By.xpath(XPATH_SELECT_DAY));
+        Select day = new Select(selectDay);
+        day.selectByIndex(5);
+
+        WebElement selectMonth = driver.findElement(By.xpath(XPATH_SELECT_MONTH));
+        Select month = new Select(selectMonth);
+        month.selectByIndex(5);
+
+        WebElement selectYear = driver.findElement(By.xpath(XPATH_SELECT_YEAR));
+        Select year = new Select(selectYear);
+        year.selectByValue("2000");
+
+    }
+}
 
